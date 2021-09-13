32cfefa6d5d8bc669d6073370ec7a54776992adb
Ibrahim sultan <Ibrahimsultan6@gmail.com>
2021-09-13 19:41:02 +0000

    work on-python-data_structures

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,9 +2,5 @@
 
 
 def element_at(my_list, idx):
-    if idx < 0 :
-        return None
-    elif idx > len(my_list):
-        return None
-    else:
+    if idx < (len(my_list)) and idx >= 0:
         return my_list[idx]
---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def replace_in_list(my_list, idx, element):
+    if idx < (len(my_list)) and idx >= 0:
+        my_list[idx] = element
+        new_list = my_list
+        return new_list
+    else:
+        return my_list
